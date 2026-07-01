dcc9cfcace80006407e72d6b2853ef94876db4c2
Tikhacheva <tikhacheva07@gmail.com>
2026-07-02 00:39:07 +0300

    style: minor changes in style

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function showWeather() {
       const temp = data.main.temp;
       const name = data.name;
       const description = data.weather[0].description;
+      gti
 
       weatherBox.innerHTML = `
         <div class="weather-info">
